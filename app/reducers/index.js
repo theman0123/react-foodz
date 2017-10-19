ccab78e17d6c15149d8c282a4ea8c8ceba7cfbae
spencer <spencajones@gmail.com>
2017-10-19 16:06:24 -0600

    setup fetch reducer for api success and errors and fetching status

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import notes from './notes.js';
 import places from './places.js';
 import user from './user.js';
+import fetch from './fetch.js';
 
 const reducer = combineReducers({
   notes,
   places,
-  user
+  user,
+  fetch
 });
 
 export default reducer;
--- app/store.js
+++ app/store.js
@@ -7,53 +7,69 @@ import reducer from './reducers/index.js';
 const middleware = applyMiddleware(thunk, logger)
 
 const store = createStore(reducer, middleware);
+/*
+//PLACE
+store.dispatch({type: 'NEW_PLACE',
+                id: '123',
+                placeName: 'Chilis',
+                address: '234 n. 234 s., slc, ut, 84111',
+                likes: 4,
+                stars: 3,
+                photo: 'somephoto',
+                categories: ['tacos', 'local', 'foodmania']
+               })
+store.dispatch({type: 'EDIT_PLACE',
+                id: '123',
+                placeName: 'Wallabys',
+                address: '543 n. 234 s., slc, ut, 23849',
+                likes: '4',
+                stars: '3',
+                photo: 'edited',
+                categories: ['tacos', 'local', 'edited']
+               })
+store.dispatch({type: 'DELETE_PLACE', id: '123'})
 
-//store.dispatch({type: 'NEW_PLACE',
-//                id: '123',
-//                placeName: 'Chilis',
-//                address: '234 n. 234 s., slc, ut, 84111',
-//                likes: 4,
-//                stars: 3,
-//                photo: 'somephoto',
-//                categories: ['tacos', 'local', 'foodmania']
-//               })
-//store.dispatch({type: 'EDIT_PLACE',
-//                id: '123',
-//                placeName: 'Wallabys',
-//                address: '543 n. 234 s., slc, ut, 23849',
-//                likes: '4',
-//                stars: '3',
-//                photo: 'edited',
-//                categories: ['tacos', 'local', 'edited']
-//               })
-//store.dispatch({type: 'NEW_NOTE',
-//                id: '123',
-//                placeId: '543',
-//                ownerId: '999',//=userId
-//                title: 'boring',
-//                foodDetails: 'static',
-//                show: false,
-//                likes: '4',
-//                stars: '3',
-//                photo: 'original',
-//                tags: ['burritos', 'local', 'edited']
-//               })
-//
-//store.dispatch({type: 'EDIT_NOTE',
-//                id: '123',
-//                placeId: '543',
-//                ownerId: '999',
-//                userId: '999',
-//                title: 'different',
-//                foodDetails: 'changed',
-//                show: true,
-//                likes: '4',
-//                stars: '3',
-//                photo: 'edited',
-//                tags: ['tacos', 'local', 'edited']
-//               })
-//store.dispatch({type: 'DELETE_NOTE', id: '123', userId: '999'})
-//store.dispatch({type: 'DELETE_PLACE', id: '123'})
-store.dispatch({type: 'LOG-IN'})
+//NOTES
+store.dispatch({type: 'NEW_NOTE',
+                id: '123',
+                placeId: '543',
+                ownerId: '999',//=userId
+                title: 'boring',
+                foodDetails: 'static',
+                show: false,
+                likes: '4',
+                stars: '3',
+                photo: 'original',
+                tags: ['burritos', 'local', 'edited']
+               })
+
+store.dispatch({type: 'EDIT_NOTE',
+                id: '123',
+                placeId: '543',
+                ownerId: '999',
+                userId: '999',
+                title: 'different',
+                foodDetails: 'changed',
+                show: true,
+                likes: '4',
+                stars: '3',
+                photo: 'edited',
+                tags: ['tacos', 'local', 'edited']
+               })
+store.dispatch({type: 'DELETE_NOTE', id: '123', userId: '999'})
+
+//LOGIN/OUT
+store.dispatch({type: 'LOG-IN',
+                id: '999',
+                username: 'something lame',
+                avatar: 'dunno about this'
+               })
+//store.dispatch({type: 'LOG-OUT'})
+
+//FETCHING
+store.dispatch({type: 'FETCH_USERS'})
+store.dispatch({type: 'FETCH_USERS_ERROR', errorMessage: 'something went wrong'})
+store.dispatch({type: 'FETCH_USERS_SUCCESS'})
+*/
 
 export default store;
